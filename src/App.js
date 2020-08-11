ad9d933eb7fd0786132003d2a4d960f33bb74811
Antony <vijaykerolin2006@gmail.com>
2020-08-12 02:42:31 +0530

    'secondCommit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='grid-container'>
+      <header className='bg-primary  text-white'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col'>
+              <a href='/' className=' text-white'>
+                React Shopping Cart
+              </a>
+            </div>
+          </div>
+        </div>
       </header>
+      <main>Product List</main>
+      <footer className='bg-primary text-white'>All rights reserved by @allocious</footer>
     </div>
   );
 }
