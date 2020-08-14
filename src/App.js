59fe86376722caaf29371773f796eb10852b54dd
Antony <vijaykerolin2006@gmail.com>
2020-08-15 00:39:41 +0530

    Form component

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
       size: '',
       sort: ''
     };
   }
 
+  createOrder = order => {
+    console.log('Need to save order for ' + order.name);
+  };
+
   removeFromCart = product => {
     const cartItems = this.state.cartItems.slice();
     this.setState({ cartItems: cartItems.filter(x => x._id !== product._id) });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems.filter(x => x._id !== product._id)));
   };
 
   addToCart = product => {
@@ -36,6 +41,7 @@ class App extends React.Component {
     this.setState({
       cartItems
     });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
 
   sortProducts = event => {
@@ -77,7 +83,7 @@ class App extends React.Component {
               <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
             <div className='sidebar'>
-              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder} />
             </div>
           </div>
         </main>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import formatCurrency from '../util';
 
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      address: '',
+      showCheckout: false
+    };
+  }
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  createOrder = e => {
+    e.preventDefault();
+    const order = {
+      name: this.state.name,
+      email: this.state.email,
+      address: this.state.address,
+      cartItems: this.props.cartItems
+    };
+    this.props.createOrder(order);
+  };
   render() {
     const { cartItems } = this.props;
     return (
@@ -29,11 +51,41 @@ export default class Cart extends Component {
             </ul>
           </div>
           {cartItems.length !== 0 && (
-            <div className='cart'>
-              <div className='total'>
-                <div className=''>Total {formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0))} </div>
-                <button className='btn btn-success'>Proceed</button>
+            <div className=''>
+              <div className='cart'>
+                <div className='total'>
+                  <div className=''>Total {formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0))} </div>
+                  <button onClick={() => this.setState({ showCheckout: true })} className='btn btn-success'>
+                    Proceed
+                  </button>
+                </div>
               </div>
+
+              {this.state.showCheckout && (
+                <div className='cart'>
+                  <form onClick={this.createOrder}>
+                    <ul className='form-container'>
+                      <li>
+                        <label>Email</label>
+                        <input type='email' name='email' required onChange={this.handleInput} />
+                      </li>
+                      <li>
+                        <label>Name</label>
+                        <input type='text' name='name' required onChange={this.handleInput} />
+                      </li>
+                      <li>
+                        <label>Address</label>
+                        <input type='text' name='address' required onChange={this.handleInput} />
+                      </li>
+                      <li>
+                        <button className='btn btn-info' type='submit'>
+                          Checkout
+                        </button>
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+              )}
             </div>
           )}
         </div>
